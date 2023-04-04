5cc09148f6928cf3e61f71ed39fe440d5c3947ea
awaismuhammad1234 <125891919+awaismuhammad1234@users.noreply.github.com>
2023-04-04 12:38:28 +0100

    Update Assignment 2, Statistics and trends, Muhammad Awais .py

--- Assignment 2, Statistics and trends, Muhammad Awais .py
+++ Assignment 2, Statistics and trends, Muhammad Awais .py
@@ -12,3 +12,28 @@ import seaborn as sns
 # Setting environment to ignore future warnings
 import warnings
 warnings.simplefilter('ignore')
+
+
+# In[2]:
+
+
+def ingest_data(filename):
+    """
+    Read the World Bank dataset and return two dataframes:
+    one with years as columns and one with countries as columns.
+    """
+    # Read dataset
+    df = pd.read_csv(filename, skiprows=4).iloc[:, :-1]
+    
+    # Transform dataset with years as columns
+    df_years = df
+
+    # Transform dataset with countries as columns
+    df_countries = df.set_index('Country Name').T
+    df_countries.index.name = 'Indicator'
+    df_countries = df_countries.reset_index()
+
+    return df_years, df_countries
+
+
+# In[3]:
