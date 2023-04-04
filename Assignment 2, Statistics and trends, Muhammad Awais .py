cc009568f9cba35409071e19706b1a750a2e9c23
awaismuhammad1234 <125891919+awaismuhammad1234@users.noreply.github.com>
2023-04-04 12:33:50 +0100

    Create Assignment 2, Statistics and trends, Muhammad Awais .py

---
+++ Assignment 2, Statistics and trends, Muhammad Awais .py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Setting environment to ignore future warnings
+import warnings
+warnings.simplefilter('ignore')
